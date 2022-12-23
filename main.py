d4ff0987551cc7e0eb28208aca607b4203e33ed7
Skullheadx <704277@pdsb.net>
2022-12-22 21:23:59 -0500

    animation stops at proper time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from setup import *
 from vector import VectorManager, Vector
-
+import time
 screen = pygame.display.set_mode((720, 720))
 
 m, d = to_polar(WIDTH / 2, HEIGHT / 2)
@@ -9,24 +9,26 @@ offset = Vector(m, d)
 clock = pygame.time.Clock()
 delta = 0
 is_running = True
-scene = VectorManager("tests/test1.txt")
+scene = VectorManager("test.txt")
+
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
-    scene.update(delta / 1000)
+    if not scene.is_finished(delta/1000):
+        scene.update(delta / 1000)
+        screen.fill(WHITE)
 
-    screen.fill(WHITE)
+        if len(scene.buffer) > 1:
+            points = []
+            for i in scene.buffer:
+                points.append((offset + i).components())
+            pygame.draw.lines(screen, BLACK, False, points, 3)
+        prev = offset
+        for i in scene.vectors:
+            pygame.draw.line(screen, RED, prev.components(), (prev + i).components(), 3)
+            prev += i
 
-    if len(scene.buffer) > 1:
-        points = []
-        for i in scene.buffer:
-            points.append((offset + i).components())
-        pygame.draw.lines(screen, BLACK, False, points, 3)
-    prev = offset
-    for i in scene.vectors:
-        pygame.draw.line(screen, RED, prev.components(), (prev + i).components(), 3)
-        prev += i
     pygame.display.flip()
     delta = clock.tick()
--- vector.py
+++ vector.py
@@ -1,9 +1,9 @@
-from math import cos, sin, hypot, atan2
-
+from math import cos, sin, hypot, atan2, pi,tan
+from utils import to_components
 
 class Vector:
 
-    def __init__(self, magnitude, direction, frequency=1):
+    def __init__(self, magnitude, direction, frequency=1.0):
         self.frequency = frequency
         self.length = magnitude
         self.angle = direction
@@ -14,7 +14,7 @@ class Vector:
         return Vector(hypot(x, y), atan2(y, x), self.frequency)
 
     def rotate(self, rad):
-        self.angle += rad * self.frequency
+        self.angle += rad * self.frequency * 2 * pi
 
     def x_component(self):
         return self.length * cos(self.angle)
@@ -28,10 +28,19 @@ class Vector:
 
 class VectorManager:
     def __init__(self, filename):
+        self.time = 0
+        self.target =1
         with open(filename, "r") as f:
             contents = f.read().split('\n')
+            mode = contents[0]
             contents = contents[1:]
-        contents = [tuple(map(int, i.split(" "))) for i in contents]
+        contents = [tuple(map(float, i.split(" "))) for i in contents]
+        if mode == "polar":
+            for i, j in enumerate(contents):
+                x,y = to_components(j[0], j[1])
+                contents[i] = (x,y,j[2])
+                self.target *= j[2]
+
         self.vectors = [Vector(i, j, k) for i, j, k in contents]
 
         self.buffer = []
@@ -41,4 +50,11 @@ class VectorManager:
         for vector in self.vectors:
             vector.rotate(delta)
             end += vector
+        # print(self.time)
         self.buffer.append(end)
+
+    def is_finished(self, time):
+        self.time += time
+        if self.time >= self.target:
+            return True
+        return False
