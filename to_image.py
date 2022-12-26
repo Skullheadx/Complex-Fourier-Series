5e72be501f7ef1c407b565e2af12a9990514d8d9
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2022-12-25 22:55:08 -0500

    generations

--- to_image.py
+++ to_image.py
@@ -1,13 +1,13 @@
 import pygame
 
 def draw():
-    WIDTH, HEIGHT = 720,720
+    WIDTH, HEIGHT = 640, 640
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
     is_running = True
-
-    with open("generations/gen30 buffer.txt", "r") as f:
+    img = pygame.image.load("images/circle.png")
+    with open("generations/gen100 buffer.txt", "r") as f:
         contents = f.read().split("\n")
     buffer = [tuple(map(float, i.split(" "))) for i in contents]
     for i in range(len(buffer)):
@@ -18,7 +18,8 @@ def draw():
                 is_running = False
 
         screen.fill((255,255,255))
-        pygame.draw.lines(screen, (0,0,0), True, buffer, 3)
+        screen.blit(img,img.get_rect(center=(WIDTH/2,HEIGHT/2)))
+        pygame.draw.lines(screen, (0,0,255), True, buffer, 1)
 
         pygame.display.flip()
         clock.tick(60)
