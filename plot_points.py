618a3b4d2e8b563a00cf1b8908d36acfea4ecaf3
lbcmk <30442978+lbcmk@users.noreply.github.com>
2022-12-28 18:51:59 -0500

    Converted into class

--- plot_points.py
+++ plot_points.py
@@ -1,17 +1,34 @@
 from svg_to_path import Reader
 
+class svgToPath():
+    
+    def __init__(self, path):
+        self.folder = ''
+        self.filename = path.split('/')[-1]
+        for i in path.split('/')[:-1]:
+            self.folder += i + '/';
 
-folder = './svgs/'
-filename = 'test3.svg'
-reader = Reader(folder + filename, 0.01);
+    def convert(self):
+        self.reader = Reader(self.folder + self.filename, 0.01);
+        self.reader.convertToPath()
 
-reader.convertToPath()
+    def writeToFile(self, path=''):
+        if(path == ''):
+            f = open(f"./paths/{self.filename.split('.')[0]}.txt", 'w')
+            [f.writelines(str(i) + "\n") for i in self.reader.tracedPath]
+            f.close()
+        else:
+            f = open(f"{path}", 'w')
+            [f.writelines(str(i) + "\n") for i in self.reader.tracedPath]
+            f.close()
 
-f = open(f"./paths/{filename.split('.')[0]}.txt", 'w')
-[f.writelines(str(i) + "\n") for i in reader.tracedPath]
-f.close()
 
-# import matplotlib.pyplot as plt
-# x, y = zip(*reader.tracedPath)
-# plt.scatter(x, y)
-# plt.show()
+class readFromPath():
+    def __init__(self, file):
+        self.pathCoords = []
+
+        f = open(file, 'r')
+        for i in range(sum(1 for j in open(file))):
+            temp = f.readline()[1:-2].split(",")
+            self.pathCoords.append([float(temp[0]), float(temp[1])])
+        f.close()
